25694a95573e41fbfbda9190a051788a02b40a73
Ryan Buchanan <buchanan.ryan22@gmail.com>
2020-09-10 19:35:07 -0600

    Bandwith too slow to continue tonight

--- Simple_Linear_Regression.py
+++ Simple_Linear_Regression.py
@@ -14,3 +14,10 @@ y = dataset.iloc[:, -1].values
 # Splitting the dataset into the Training set and the Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training the Simple Linear Regression Model on the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+Predicting the Test Set reults
