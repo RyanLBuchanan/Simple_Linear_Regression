e9df6e223497bdd15e7949cee3091c8222cb489f
Ryan Buchanan <buchanan.ryan22@gmail.com>
2020-09-10 19:23:41 -0600

    break

--- Simple_Linear_Regression.py
+++ Simple_Linear_Regression.py
@@ -1,3 +1,16 @@
 # Simple Linear Regression from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 10SEP20
 
+# Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and the Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
